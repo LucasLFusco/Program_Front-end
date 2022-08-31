6b6056f3a1f1b8bcc1c15ae9877c7984c95ba369
LucasLFusco <lucas_fusco1@outlook.com>
2022-08-31 20:16:40 -0300

    Primeiro commit

--- Códigos/Aula 3/script3_01.js
+++ Códigos/Aula 3/script3_01.js
@@ -3,10 +3,10 @@ let bin = 0b10, hexa = 0x10;
 let dia, nota, cep;
 dia = 30;
 nota = 9.7;
-cep = "03350123";
-document.write (dia);//25
+cep = "06665-020";
+document.write (dia);//30
 document.write("<br>");//quebra de linha
 document.write (nota);//9.7
 document.write("<br>");//quebra de linha
-document.write(oct,"<br>",bin,"<br>",hexa,"<br>",);
+document.write(oct,"<br>",bin,"<br>",hexa,"<br>",);//número octal (0-7), número binario(0 e 1), número hexadecimal//
 document.write("cep: ",cep);
