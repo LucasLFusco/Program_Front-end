9f7dccc4f2eef3d21ab97719976718cc9258fe6a
LucasLFusco <lucas_fusco1@outlook.com>
2022-09-06 22:15:17 -0300

    primeiro Commit

---
+++ Códigos/Aula 3/script3_02.js
@@ -0,0 +1,18 @@
+let aluno1, nota1, aluno2, nota2, tag; //variáveis 
+aluno1  = "Suposto Lobisomen"; //valor da variavel aluno1
+nota1   = "2.75";             //valor da variavel nota 1
+aluno2  = "Filho do bill";
+nota2   = "10";
+tag     = "<hr>";
+
+document.write ("Aluno: ",aluno1, "<br> Nota: ",nota1,tag);
+document.write ("Aluno: ",aluno2, "<br> Nota: ",nota2,);
+/*
+document.write ("aluno: ",aluno);
+document.write (tag);
+document.write ("nota: ", nota);
+document.write ("<hr>");
+document.write ("aluno: ",aluno1);
+document.write (tag);
+document.write ("nota: ", nota2);
+*/
